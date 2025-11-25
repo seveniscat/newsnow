f96613963342f8001ba8d4d44c497c4b7413a6e9
zhangbing <zhangbing@bloks.com>
2025-11-25 17:14:18 +0800

    remove logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -50,18 +50,18 @@ export function Header() {
     <>
       <span className="flex justify-self-start">
         <Link to="/" className="flex gap-2 items-center">
-          <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
-          <span className="text-2xl font-brand line-height-none!">
+          {/* <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} /> */}
+          {/* <span className="text-2xl font-brand line-height-none!">
             <p>News</p>
             <p className="mt--1">
               <span className="color-primary-6">N</span>
               <span>ow</span>
             </p>
-          </span>
+          </span> */}
         </Link>
-        <a target="_blank" href={`${Homepage}/releases/tag/v${Version}`} className="btn text-sm ml-1 font-mono">
+        {/* <a target="_blank" href={`${Homepage}/releases/tag/v${Version}`} className="btn text-sm ml-1 font-mono">
           {`v${Version}`}
-        </a>
+        </a> */}
       </span>
       <span className="justify-self-center">
         <span className="hidden md:(inline-block)">
@@ -71,8 +71,8 @@ export function Header() {
       <span className="justify-self-end flex gap-2 items-center text-xl text-primary-600 dark:text-primary">
         <GoTop />
         <Refresh />
-        <Github />
-        <Menu />
+        {/* <Github /> */}
+        {/* <Menu /> */}
       </span>
     </>
   )
